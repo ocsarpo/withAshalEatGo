6bf18b103262c0a5591fa9954d12ea7f853235f2
ocsarpo <ocsarpo22@gmail.com>
2020-08-25 00:14:51 +0900

    공통으로 사용할 도메인 모델을 도메인패키지로 분리

--- eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgo/EatgoAdminApiApplicationTests.java
+++ eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgo/EatgoAdminApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class EatgoApplicationTests {
+public class EatgoAdminApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
