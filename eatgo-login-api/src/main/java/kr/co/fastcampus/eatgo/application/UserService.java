f60bb40d0bb9c876451dab5a3b2b0c038568c943
ocsarpo <ocsarpo22@gmail.com>
2020-09-05 17:34:35 +0900

    Login API 분리

--- eatgo-login-api/src/main/java/kr/co/fastcampus/eatgo/application/UserService.java
+++ eatgo-login-api/src/main/java/kr/co/fastcampus/eatgo/application/UserService.java
@@ -3,8 +3,6 @@ package kr.co.fastcampus.eatgo.application;
 import kr.co.fastcampus.eatgo.domain.User;
 import kr.co.fastcampus.eatgo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,24 +22,15 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User registerUser(String email, String name, String password) {
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EmailNotExistedException(email));
 
-        Optional<User> existed = userRepository.findByEmail(email);
-        
-        if(existed.isPresent()) {
-            throw new EmailExistedException(email);
+//        사용자입력값과, 인코드된 값을 비교함
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new PasswordWrongException();
         }
 
-        String encodedPassword =
-                passwordEncoder.encode(password);
-
-        User user = User.builder()
-                .email(email)
-                .name(name)
-                .password(encodedPassword)
-                .level(1L)
-                .build();
-
-        return userRepository.save(user);
+        return user;
     }
 }
