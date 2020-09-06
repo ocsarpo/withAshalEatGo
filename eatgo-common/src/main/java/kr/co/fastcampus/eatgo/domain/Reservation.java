7500125ed66ac61603b66a873ebda420c34bb9dd
ocsarpo <ocsarpo22@gmail.com>
2020-09-06 19:09:30 +0900

    예약 기능
     - 컨트롤러 생성
     - 컨트롤러 테스트 생성
     - 컨트롤러 테스트 create 생성, mvc.perform-> status().isCreated() 확인
     - 컨트롤러 create 작성, ReponseEntity.created(new URI(url)).body("{}") 리턴
     - 테스트에서 create에 필요한  인자 전달 상황 가정 작성 -> Service 클래스 생성 , Service클래스의 addReservation 함수 생성 : verify
     - 컨트롤러에서 임시 인자로 Service에 전달  테스트확인
     - 예약 도메인 생성
     - 임시 인자 사용했던 것을 도메인.get*()로 변경
     - jwt토큰을 헤더에 넣는 테스트 작성
     - 컨트롤러에서 claims로 userId와 name을 가져오도록 함
    
    가게에 대한 예약이 들어감
    그 다음 -> 실제 예약 기능 (Service의 addReservation 함수를 작성!)

---
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Reservation.java
@@ -0,0 +1,39 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+// 프로그램 내부에서 넣어주는 값이기 때문에 validation 하지않음
+    private Long userId;
+
+// 프로그램 내부에서 넣어주는 값이기 때문에 validation 하지않음
+    private String name;
+
+    @NotEmpty
+    @Setter
+    private String date;
+
+    @NotEmpty
+    @Setter
+    private String time;
+
+    @NotNull
+    @Setter
+    private Integer partySize;
+
+}
---
+++ eatgo-customer-api/src/main/java/kr/co/fastcampus/eatgo/application/ReservationService.java
@@ -0,0 +1,14 @@
+package kr.co.fastcampus.eatgo.application;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ReservationService {
+    public void addReservation(Long restaurantId, Long userId, String name,
+                               String date, String time, Integer partSize) {
+        //    TODO
+    }
+}
